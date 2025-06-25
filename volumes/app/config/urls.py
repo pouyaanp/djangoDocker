06902fb582786c09530e3eaeea321e788e6e2fe9
pouya <pouyaanp0@gmail.com>
2025-06-26 01:35:11 +0330

    create a new volume directory

--- volumes/app/config/urls.py
+++ volumes/app/config/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for app project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
@@ -28,4 +27,3 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
--- volumes/app/web/views.py
+++ volumes/app/web/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>hello this is test for learning dockerising django<h1>")
+    return HttpResponse("<h1>hello this is test for learning dockerising django and mysql<h1>")
